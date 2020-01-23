ebb9a2a6b6a69e72b395949115c68d16cfe8c86a
John Michael San Diego <jmsandiego0710@gmail.com>
2020-01-23 16:05:55 +0800

    Added the storyboard for tab bar items and map

---
+++ NPFoodStallReview/MainController.swift
@@ -0,0 +1,16 @@
+//
+//  MainController.swift
+//  NPFoodStallReview
+//
+//  Created by Jm San Diego on 23/1/20.
+//  Copyright © 2020 Jm San Diego. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
