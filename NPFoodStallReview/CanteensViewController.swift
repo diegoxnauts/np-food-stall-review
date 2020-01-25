dfbf1911fdc03db713f557c500a1a25b41aab799
John Michael San Diego <jmsandiego0710@gmail.com>
2020-01-25 13:08:56 +0800

    can fetch canteen and display in the table view

--- NPFoodStallReview/CanteensViewController.swift
+++ NPFoodStallReview/CanteensViewController.swift
@@ -14,18 +14,40 @@ import MapKit
 class CanteensViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var canteensTableView: UITableView!
+    
+    var canteenController: CanteenController = CanteenController();
+    var stallController: StallController = StallController();
+    var feedbackController: FeedbackController = FeedbackController();
+    
+    var canteenList: [Canteen] = [];
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        canteensTableView.delegate = self;
+        
+        // load the canteens
+        canteenController.retrieveCanteens() {(canteens) -> () in
+            for canteen in canteens {
+                self.canteenList.append(canteen);
+            }
+            
+            self.canteensTableView.reloadData();
+        }
+        
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10;
+        return canteenList.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CanteenCell", for: indexPath)
-        cell.textLabel?.text = "Canteen"
+        
+        var canteen = canteenList[indexPath.row];
+        cell.textLabel?.text = canteen.name;
         return cell
     }
 }
