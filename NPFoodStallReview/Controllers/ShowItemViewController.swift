790b921e6d737455f17560faad7136b4b97a4bbe
John Michael San Diego <jmsandiego0710@gmail.com>
2020-01-29 14:39:38 +0800

    - WIP Item Feature

---
+++ NPFoodStallReview/Controllers/ShowItemViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ShowItemViewController.swift
+//  NPFoodStallReview
+//
+//  Created by Jm San Diego on 29/1/20.
+//  Copyright © 2020 Jm San Diego. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ShowItemViewController: UIViewController {
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad();
+    }
+}
