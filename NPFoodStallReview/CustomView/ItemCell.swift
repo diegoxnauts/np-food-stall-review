bf8ad7945ff000933474bba3845b38176229633b
John Michael San Diego <jmsandiego0710@gmail.com>
2020-01-30 18:13:05 +0800

    -added item cell

---
+++ NPFoodStallReview/CustomView/ItemCell.swift
@@ -0,0 +1,32 @@
+//
+//  ItemCell.swift
+//  NPFoodStallReview
+//
+//  Created by Jm San Diego on 29/1/20.
+//  Copyright © 2020 Jm San Diego. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemCell : UITableViewCell {
+
+    @IBOutlet weak var itemName: UILabel!
+    @IBOutlet weak var itemPrice: UILabel!
+    @IBOutlet weak var likesNo: UILabel!
+    @IBOutlet weak var likeBtn: UIButton!
+    @IBOutlet weak var unlikeBtn: UIButton!
+    
+    func setup(item: Item) {
+        itemName.text = item.name
+        itemPrice.text = convertDoubleToCurrency(amount: item.price)
+
+    }
+    
+    func convertDoubleToCurrency(amount: Double) -> String{
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .currency
+        numberFormatter.locale = Locale.current
+        return numberFormatter.string(from: NSNumber(value: amount))!
+    }
+}
